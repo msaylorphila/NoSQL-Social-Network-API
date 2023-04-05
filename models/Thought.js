7ce99601de38354db546486df16bd61a2fc0bf3b
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-04-05 12:31:48 -0400

    models and file structure created

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => {
+                return new Date(timestamp).toLocaleString();
+            }
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought
